070d2b8621384aa498e411ec095db1faf1c8aab2
Noane Dan <Noane.Dan@outlook.com>
2017-10-04 19:01:15 +0300

    Add validation to form

--- App/Component/SignUpScreen.js
+++ App/Component/SignUpScreen.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { View, Text, TextInput, Button } from 'react-native';
 
 import SignUpStyle from './../Style/SignUpStyle';
+import SignUpService from './../Service/SignUpService';
 
 export default class SignUpScreen extends Component {
 
@@ -13,12 +14,27 @@ export default class SignUpScreen extends Component {
 
     super(props);
 
+    this.service = new SignUpService();
     this.state = {
       name: '',
       username: '',
       password: '',
+      repassword: '',
       email: '',
     };
+  }
+
+  _signUp() {
+
+    let { name, username, password, repassword, email } = this.state;
+    let validationResult = this.service.validate({ name, username, password, repassword, email });
+
+    if (!validationResult.valid) {
+      this.setState({ error: validationResult.message });
+      return;
+    }
+
+    this.setState({ error: '' });
   }
 
   render() {
@@ -36,32 +52,42 @@ export default class SignUpScreen extends Component {
           <TextInput
             style={SignUpStyle.bodyElement}
             value={this.state.name}
-            onChangeText={() => {}}/>
+            onChangeText={(text) => { this.setState({ name: text }) }}/>
 
           <Text style={SignUpStyle.bodyElement}>Username:</Text>
           <TextInput
             style={SignUpStyle.bodyElement}
             value={this.state.username}
-            onChangeText={() => {}}/>
+            onChangeText={(text) => { this.setState({ username: text }) }}/>
 
           <Text style={SignUpStyle.bodyElement}>Password:</Text>
           <TextInput
             style={SignUpStyle.bodyElement}
             secureTextEntry={true}
             value={this.state.password}
-            onChangeText={() => {}}/>
+            onChangeText={(text) => { this.setState({ password: text }) }}/>
+
+            <Text style={SignUpStyle.bodyElement}>Confirm Password:</Text>
+            <TextInput
+              style={SignUpStyle.bodyElement}
+              secureTextEntry={true}
+              value={this.state.repassword}
+              onChangeText={(text) => { this.setState({ repassword: text }) }}/>
 
           <Text style={SignUpStyle.bodyElement}>Email:</Text>
           <TextInput
             style={SignUpStyle.bodyElement}
             value={this.state.email}
-            onChangeText={() => {}}/>
+            keyboardType='email-address'
+            onChangeText={(text) => { this.setState({ email: text }) }}/>
+
+          <Text style={SignUpStyle.bodyElement}>{this.state.error}</Text>
         </View>
 
         <View style={SignUpStyle.footer}>
           <Button
             title="Send"
-            onPress={() => {}}>
+            onPress={() => { this._signUp() }}>
           </Button>
         </View>
       </View>
