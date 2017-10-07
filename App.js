70b331a7772677b9a51a2b00af5a1683ff47868c
Noane Dan <Noane.Dan@outlook.com>
2017-10-07 17:10:53 +0300

    Map naviagtion props to component props

--- App.js
+++ App.js
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { AppRegistry } from 'react-native';
 import { StackNavigator } from 'react-navigation';
 
 import LogInScreen from './App/Component/LogInScreen';
 import SignUpScreen from './App/Component/SignUpScreen';
 
+const mapNavigationStateParamsToProps = (SomeComponent) => {
+  return class extends Component {
+    static navigationOptions = SomeComponent.navigationOptions;
+      render() {
+        const {navigation: {state: {params}}} = this.props
+        return <SomeComponent {...params} {...this.props} />
+    }
+  }
+}
+
 const App = StackNavigator({
-  LogIn: { screen: LogInScreen },
-  SignUp: { screen: SignUpScreen },
+  LogIn: { screen: mapNavigationStateParamsToProps(LogInScreen) },
+  SignUp: { screen: mapNavigationStateParamsToProps(SignUpScreen) },
 },
 {
   headerMode: 'none'
