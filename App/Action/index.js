61646385ffd7032779f1674e3ac1d68630536c64
Noane Dan <Noane.Dan@outlook.com>
2017-10-07 19:58:29 +0300

    Add user state actions

---
+++ App/Action/index.js
@@ -0,0 +1,10 @@
+
+export const setLogInStatus = (status) => ({
+  type: 'SET_LOG_IN_STATUS',
+  value: status
+});
+
+export const setAccessToken = (accessToken) => ({
+  type: 'SET_ACCESS_TOKEN',
+  value: accessToken
+});
