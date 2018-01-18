250e47c9983ad20769bd6d5c2b9d3cb5c735047b
Noane Dan <Noane.Dan@outlook.com>
2018-01-18 14:02:46 +0200

    Save Workout

---
+++ App/Component/SaveWorkout.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export default class SaveWorkout extends Component {
+
+    constructor(props) {
+
+        super(props);
+    }
+
+    render() {
+        return (
+        );
+    }
+}
