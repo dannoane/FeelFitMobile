f04fe56db35f3fa75fb874de1717d87f7d3771bb
Noane Dan <Noane.Dan@outlook.com>
2018-03-09 20:31:50 +0200

    Extract Geolocation as a separate service

---
+++ App/Util/Geolocation.js
@@ -0,0 +1,33 @@
+import { store } from './Store';
+import { addPosition, addMovementData } from './../Action';
+
+let watchId = null;
+store.subscribe(() => {
+
+  let route = store.getState().Route;
+
+  if (route.workoutState === 'started' && watchId === null) {
+    watchId = navigator.geolocation.watchPosition((data) => {
+
+      let { latitude, longitude, ...movementData } = data.coords;
+
+      if (movementData.accuracy <= 30) {
+        store.dispatch(addPosition({ latitude, longitude }));
+        store.dispatch(addMovementData(movementData));
+      }
+    },
+    (err) => {
+      console.log(err.code, err.message);
+    },
+    {
+      timeout: 25000,
+      maximumAge: 10000,
+      enableHighAccuracy: true,
+      distanceFilter: 5
+    });
+  }
+  else if (route.workoutState !== 'started' && watchId !== null) {
+    navigator.geolocation.clearWatch(watchId);
+    watchId = null;
+  }
+});
