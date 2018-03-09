c5eb3182266c94d90ee44ee342e1abb9f45b0359
Noane Dan <Noane.Dan@outlook.com>
2018-03-09 20:32:19 +0200

    Extract Weather fetching as a service

---
+++ App/Util/Weather.js
@@ -0,0 +1,24 @@
+import _ from 'lodash';
+import { store } from './Store';
+import { setWeather } from './../Action';
+import WeatherService from './../Service/WeatherService';
+
+const UPDATE_INTERVAL = 1000 * 60 * 15 * 1000;
+let lastUpdated = null;
+let service = new WeatherService();
+
+store.subscribe(async () => {
+
+  let route = store.getState().Route;
+
+  if (route.workoutState === 'started' && route.hasLocation) {
+    if (lastUpdated === null || route.time - lastUpdated > UPDATE_INTERVAL) {
+      lastUpdated = Date.now();
+
+      let location = _.last(_.last(route.route));
+      let weather = await service.getWeather(location);
+
+      store.dispatch(setWeather(weather));
+    }
+  }
+});
