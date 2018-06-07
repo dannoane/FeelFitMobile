639e11c1a07f529e1c2873c5363cc05059dfb40b
Noane Dan <Noane.Dan@outlook.com>
2018-06-07 21:06:26 +0300

    Add  new font styles

--- App/Style/FontStyle.js
+++ App/Style/FontStyle.js
@@ -10,3 +10,15 @@ export const bigIndicatorStyle = {
     fontWeight: '600',
     fontFamily: 'sans-serif-condensed'
 };
+export const detailTitleStyle = {
+    color: 'black',
+    fontSize: 20,
+    fontWeight: '400',
+    fontFamily: 'sans-serif-condensed'
+};
+export const detailValueStyle = {
+    color: 'grey',
+    fontSize: 25,
+    fontWeight: '400',
+    fontFamily: 'sans-serif-condensed'
+};
