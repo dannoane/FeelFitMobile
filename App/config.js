fcc4d6b74e84e449b7194d107e423cb422474811
Noane Dan <Noane.Dan@outlook.com>
2018-06-07 21:07:52 +0300

    Add config file

---
+++ App/config.js
@@ -0,0 +1,4 @@
+export const api = 'http://feelfit.westeurope.cloudapp.azure.com:8080';
+
+export const authorization = 'Bearer cN1Ug8bw1zprUKBrw6cUCjk3nRnn7Kqjh1Hwa/sI2tlUxDCrenc7yk022lDvBBIFFNgUHHnyBkot04pn9rUCfw==';
+export const classifier = 'https://ussouthcentral.services.azureml.net/workspaces/fd6e7488ccbf4d0e8e975d2f7005dfb2/services/438e15c4d32542a8ad036b27c45031e8/execute?api-version=2.0&details=true';
