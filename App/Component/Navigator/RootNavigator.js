c2ac457e003d9f8f73f0cfe5dd7ffce5985c8eae
Noane Dan <Noane.Dan@outlook.com>
2018-06-17 16:24:29 +0300

    Fix back button

--- App/Component/Navigator/RootNavigator.js
+++ App/Component/Navigator/RootNavigator.js
@@ -26,7 +26,7 @@ RootNavigator.router.getStateForAction = (action, state) => {
   if (
     state &&
     action.type === NavigationActions.BACK &&
-    !state.routes[state.index].routeName.equals('SignUp') && !state.routes[state.index - 1].routeName.equals('LogIn')
+    !['SignUp', 'LogIn', 'FollowWorkout', 'Track', 'MyStatistics', 'SearchWorkouts'].includes(state.routes[state.index].routeName)
   ) {
     return null;
   }
