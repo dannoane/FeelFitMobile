1d4487858414aedc34a0861ad49ba539e8d488b4
Noane Dan <Noane.Dan@outlook.com>
2018-06-30 15:08:58 +0300

    Add voice library

--- android/app/src/main/java/com/FeelFit/MainApplication.java
+++ android/app/src/main/java/com/FeelFit/MainApplication.java
@@ -3,6 +3,7 @@ package com.FeelFit;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import net.no_mad.tts.TextToSpeechPackage;
 import com.airbnb.android.react.maps.MapsPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.sensors.RNSensorsPackage;
@@ -30,6 +31,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new TextToSpeechPackage(),
           new RNSensorsPackage(),
           new VectorIconsPackage(),
           new MapsPackage(),
